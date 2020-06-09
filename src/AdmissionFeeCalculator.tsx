4e75455903c1e3f6044f13f68d7609ef2b1e15ba
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2020-06-09 23:29:40 +0900

    枠作成

---
+++ src/AdmissionFeeCalculator.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Detail } from './Detail'
+import { Summary } from './Summary'
+
+export class AdmissionFeeCalculator extends React.Component {
+  render() {
+    return (
+      <>
+        <Detail />
+        <Summary />
+      </>
+    )
+  }
+}
