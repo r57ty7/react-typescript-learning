c1ce146ea2481182955745959cb62a474f2aea6c
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2020-06-10 22:37:04 +0900

    htmlFor

--- src/AdmissionFeeCalculator.tsx
+++ src/AdmissionFeeCalculator.tsx
@@ -4,19 +4,52 @@ import { Summary } from './Summary'
 import { DetailProps } from './feeClassification'
 
 export class AdmissionFeeCalculator extends React.Component {
-  private detail: DetailProps = {
-    classification: {
-      name: '大人',
-      description: '',
-      numOfPeople: 0,
-      totalPrice: 1000,
-      unitPrice: 0,
+  private detail: DetailProps[] = [
+    {
+      classification: {
+        name: '大人',
+        description: '',
+        numOfPeople: 0,
+        totalPrice: 1000,
+        unitPrice: 0,
+      },
     },
-  }
+    {
+      classification: {
+        name: '学生',
+        description: '中学生・高校生',
+        unitPrice: 700,
+        numOfPeople: 0,
+        totalPrice: 0,
+      },
+    },
+    {
+      classification: {
+        name: '子ども',
+        description: '小校生',
+        unitPrice: 300,
+        numOfPeople: 0,
+        totalPrice: 0,
+      },
+    },
+    {
+      classification: {
+        name: '幼児',
+        description: '未就学',
+        unitPrice: 0,
+        numOfPeople: 0,
+        totalPrice: 0,
+      },
+    },
+  ]
+
   render() {
+    const detailsJsx = this.detail.map((fc, idx) => {
+      return <Detail key={idx.toString()} classification={fc.classification} />
+    })
     return (
       <>
-        <Detail classification={this.detail.classification} />
+        {detailsJsx}
         <Summary />
       </>
     )
