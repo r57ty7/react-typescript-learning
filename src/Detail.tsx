f6684564c6fe2614b52cb8b81719abb55b8b9c68
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2020-06-11 22:42:33 +0900

    関数型コンポーネント

--- src/Detail.tsx
+++ src/Detail.tsx
@@ -1,45 +1,30 @@
 import React from 'react'
 import { DetailProps, DetailState } from './types'
 
-export class Detail extends React.Component<DetailProps, DetailState> {
-  constructor(props: DetailProps) {
-    super(props)
-    this.state = {
-      numOfPeople: props.classification.numOfPeople,
-    }
-  }
-
-  onNumOfPeopleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+export const Detail: React.FC<DetailProps> = (props) => {
+  const onNumOfPeopleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const num: number = Number(e.target.value)
-    this.props.onNumOfPeopleChange(num)
+    props.onNumOfPeopleChange(num)
   }
 
-  render() {
-    return (
-      <div>
-        <div className="classification-name">
-          {this.props.classification.name}
-        </div>
-        <div className="description">
-          {this.props.classification.description}
-        </div>
-        <div className="unit-price">
-          {this.props.classification.unitPrice}円
-        </div>
-        <div className="num-people">
-          <select
-            value={this.props.classification.numOfPeople}
-            onChange={(e) => this.onNumOfPeopleChange(e)}
-          >
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-          </select>
-          <span>名</span>
-        </div>
+  return (
+    <div>
+      <div className="classification-name">{props.classification.name}</div>
+      <div className="description">{props.classification.description}</div>
+      <div className="unit-price">{props.classification.unitPrice}円</div>
+      <div className="num-people">
+        <select
+          value={props.classification.numOfPeople}
+          onChange={(e) => onNumOfPeopleChange(e)}
+        >
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+        <span>名</span>
       </div>
-    )
-  }
+    </div>
+  )
 }
--- src/Summary.tsx
+++ src/Summary.tsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import { SummaryProps } from './types'
 
-export class Summary extends React.Component<SummaryProps, {}> {
-  render() {
-    return (
-      <div>
-        <div className="party">
-          <input type="text" className="party" value={this.props.numOfPeople} />
-          <span>名様</span>
-        </div>
-        <div className="total-amount">
-          <span>合計</span>
-          <input
-            type="text"
-            className="total-amount"
-            value={this.props.totalAmount}
-          />
-          <span>円</span>
-        </div>
+export const Summary: React.FC<SummaryProps> = (props) => {
+  return (
+    <div>
+      <div className="party">
+        <input type="text" className="party" value={props.numOfPeople} />
+        <span>名様</span>
       </div>
-    )
-  }
+      <div className="total-amount">
+        <span>合計</span>{' '}
+        <input type="text" className="total-amount" value={props.totalAmount} />
+        <span>円</span>
+      </div>
+    </div>
+  )
 }
